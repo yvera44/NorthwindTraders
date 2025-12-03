672e6da687b872f2a282c87aa6cff81ad40555c7
YairVera <yairvera44@gmail.com>
2025-12-03 11:00:29 -0600

    add model

---
+++ src/main/java/com/northwind/model/Customer.java
@@ -0,0 +1,16 @@
+package com.northwind.model;
+
+public class Customer {
+
+   private String customerId;
+   private String companyName;
+   private String contactName;
+   private String address;
+   private String city;
+   private String region;
+   private String postalCode;
+   private String country;
+   private String phone;
+   private String fax;
+
+}
